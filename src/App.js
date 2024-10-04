079da5b2217635c1c60a7dbf49d592673b38a9af
William Lopera <lopera.william@gmail.com>
2024-10-04 15:43:41 -0500

    documentacion star-rating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import { Accordion } from "./components/accordion";
 
-import { StarRating } from "./components/star_rating";
+import { StarRating } from "./components/star-rating";
 
 // import { RandomColor } from "./components/random-color";
 function App() {
